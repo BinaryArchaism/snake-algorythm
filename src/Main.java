d14de419e65acc2aa61c8927b06f20ab3d320e33
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-04 19:38:34 +0300

    notWorkingCopy

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,47 @@
 
 public class Main {
     public static void main(String[] args) {
-        int N = 7;
-        // Переменная way отвечает за направление заполнения матрицы
-        // значение 1 - вправо, 2 - вниз, 3 - влево, 4 - вверх
-        int way = 1;
-        // Переменная k счетчик и число, которое заносится в матрицу
-        int k = 0;
-
+        int N = 7; // Размер матрицы
         int matrix[][] = new int[N][N];
 
-        
+        // Переменная k счетчик и число, которое заносится в матрицу
+        int k = 0;
+        for (int i = 0; i < N; ++i) {
+            matrix[0][i] = k;
+            ++k;
+        }
+
+        int down = 1;
+        int left = 6;
+        int up = 0;
+        int right = 1;
+
+
+
+        while (k <= N*N) {
+            for (int i = down; i < N; ++i) {
+                matrix[i][N - down] = k;
+                ++k;
+            } ++down;
+            for (int i = N-down; i >= 0; --i) {
+                matrix[left][N-1 - i] = k;
+                ++k;
+            } --left;
+            for (int i = N-1 - up; i >= 0; --i) {
+                matrix[i][up] = k;
+                ++k;
+            } ++up;
+            for (int i = right; i < N - down; ++i) {
+                matrix[right][i] = k;
+                ++k;
+            } ++right;
+        }
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                System.out.print(matrix[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 }
