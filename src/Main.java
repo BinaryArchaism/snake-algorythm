84c3610a4ca22307317cb5c94e535089ec053126
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-04 18:31:28 +0300

    commit#1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@
  * @author Dmitrii "BinaryArchaism" Dmitriev
  */
 
-
 public class Main {
     public static void main(String[] args) {
+        int N = 7;
+        // Переменная way отвечает за направление заполнения матрицы
+        // значение 1 - вправо, 2 - вниз, 3 - влево, 4 - вверх
+        int way = 1;
+        // Переменная k счетчик и число, которое заносится в матрицу
+        int k = 0;
+
+        int matrix[][] = new int[N][N];
+
         
     }
 }
