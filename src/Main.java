d5beb4dd2ca02a2c5a60cfe02635f7935924e840
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-06 22:21:21 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,34 @@
 
 public class Main {
     public static void main(String[] args) {
-        int N = 7; // Размер матрицы
+        int N = 5; // Размер матрицы
         int matrix[][] = new int[N][N];
 
         // Переменная k счетчик и число, которое заносится в матрицу
-        int k = 0;
-        for (int i = 0; i < N; ++i) {
-            matrix[0][i] = k;
-            ++k;
+        int k = 1;
+        int f = 0;
+        if (N%2!=0) f = 1;
+
+        for (int i = 0; i < (N + 1) / 2; ++i) {
+            for (int j = i; j < N - i - 1; ++j) {
+                matrix[i][j] = k;
+                ++k;
+            }
+            for (int j = i; j < N - i - 1; ++j) {
+                matrix[j][N - i - 1] = k;
+                ++k;
+            }
+            for (int j = N-i-1; j > i; --j) {
+                matrix[N-1-i][j] = k;
+                ++k;
+            }
+            for (int j = N-1-i; j > i; --j) {
+                matrix[j][i] = k;
+                ++k;
+            }
         }
-
-        int down = 1;
-        int left = 6;
-        int up = 0;
-        int right = 1;
-
-
-
-        while (k <= N*N) {
-            for (int i = down; i < N; ++i) {
-                matrix[i][N - down] = k;
-                ++k;
-            } ++down;
-            for (int i = 0; i <= N-down; ++i) {
-                matrix[left][N-down - i] = k;
-                ++k;
-            } --left;
-            for (int i = N-1 - up; i >= 0; --i) { // ОШИБКА!!!
-                matrix[i][up] = k;
-                ++k;
-            } ++up;
-            for (int i = right; i < N - down; ++i) {
-                matrix[right][i] = k;
-                ++k;
-            } ++right;
-        }
+        if (f == 1)
+        matrix[(N+1)/2 - 1][(N+1)/2 - 1] = N*N;
 
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
